9b9f8aa52711a9d6083102d79fb69860f7a78cd0
Keith <165685348+dontslamthedoor@users.noreply.github.com>
2024-12-06 23:20:04 -0500

    Update main.go

--- main.go
+++ main.go
@@ -14,9 +14,11 @@ func main() {
 	}
 	defer res.Body.Close()
 
-	println(res.Body)
-
-	fmt.Printf("response body: %v", res.Body)
+	body, err := ioutil.ReadAll(res.Body)
+    	if err != nil {
+        	log.Fatalf("Failed to read response body: %v", err)
+    	}
+    		fmt.Printf("response body: %s\n", body)
 
 	http.HandleFunc("/helloKeith", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi, Keith! this is net/http\n")
